a5724b2953e3c5ed1507af8191f082e8297da580
tianyu5177 <zhangtianyu5177@sina.com>
2020-11-28 15:53:56 +0800

    更新

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//引入React核心库
+import React,{Component} from 'react'
+import Hello from './components/Hello'
+
+export default class App extends Component{
+	render(){
+		return (
+			<div>
+				<Hello/>
+			</div>
+		)
+	}
+}
